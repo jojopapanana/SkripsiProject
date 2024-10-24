19b9ccd36b1151e8a90ff3ecc364c3f6441091fc
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-10-24 22:33:32 +0700

    adjusting stok.blade after changing to bootstrap 4.5.2

--- resources/views/stok.blade.php
+++ resources/views/stok.blade.php
@@ -147,6 +147,7 @@
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
         <script>
             document.querySelectorAll('form').forEach(function(form) {
@@ -283,14 +284,12 @@
                         </center>
                         <h4 class="fw-bold text-center" id="modalText">Default Text</h4>
                         <div class="d-flex justify-content-center gap-4 mt-4">
-                            <button class="btn fw-semibold" style="border: 2px solid black; width: 5vw" data-bs-dismiss="modal">Oke</button>
+                            <button class="btn fw-semibold" style="border: 2px solid black; width: 5vw" data-dismiss="modal">Oke</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
         <!-- Function to change modal text and show the modal -->
         <script>
